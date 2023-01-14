ab192b559e0f1dd62a4b3b26ec006687efbf8c44
ZixunHuang1997 <zixun@berkeley.edu>
2023-01-14 14:13:05 -0500

    first commit

---
+++ notification/views.py
@@ -0,0 +1,27 @@
+# Create your views here.
+from django.shortcuts import render
+from rest_framework import (generics, mixins, permissions, status, views,
+                            viewsets)
+from rest_framework.response import Response
+
+
+# Create your views here.
+def openNotification():
+    print('openNotification!')
+def mailSend():
+    print('mailSend!')
+def openDoc():
+    print('openDoc!')
+
+
+class Notify(views.APIView):
+    def post(self, request, format=None):
+        print(request.data)
+        openNotification()
+        return Response(status=status.HTTP_201_CREATED)
+
+class Email(views.APIView):
+    def post(self, request, format=None):
+        print(request.data)
+        mailSend()
+        return Response(status=status.HTTP_201_CREATED)
